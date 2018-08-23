96a8dc4aed174fa4802b8b755b8024ed3b5da686
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-08-23 16:45:25 +0300

    Menu Final

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,4 @@
-// Normal Clicks
+// Normal Clicks Menu
 $(function() {
     $('.hamburger').click(function() {
       $('body').toggleClass('show-nav');
@@ -6,11 +6,17 @@ $(function() {
     });
     
   });
-  
-  
+ 
   // Toggle with hitting of ESC
   $(document).keyup(function(e) {
       if (e.keyCode == 27) {
      $('body').toggleClass('show-nav');
     }
   });
+
+  $(document).ready(function() {
+    $('.btn--down').click(function(){
+    $('html, body').animate({scrollTop:$('.services').height() + 20}, 'slow');
+    return false;
+    });
+});
